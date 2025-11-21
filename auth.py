3f286894f8eebba2cbedb23f5cf1744caa052a04
Ashvinee <ashvineechatoo@icloud.com>
2025-11-21 14:58:29 +0400

    updated auth..py

--- auth.py
+++ auth.py
@@ -1,1 +1,14 @@
-print("Hello this is a test by Ashvinee")
+import bcrypt
+import os
+
+#def hash_password(plain_text_password):
+    #password_bytes = plain_text_password.encode('utf-8')
+ 	#salt = bcrypt.gensalt()
+ 	#hashed_password = bcrypt.hashpw(password_bytes, salt)
+	#return hashed_password
+
+def hash_password(plain_text_password):
+    password_bytes = plain_text_password.encode('utf-8')
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password_bytes, salt)
+    return hashed_password.decode('utf-8')
