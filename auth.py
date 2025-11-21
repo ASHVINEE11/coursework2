b76f9b0d5a7eca3939bbb872b117a081098a6fe9
Ashvinee <ashvineechatoo@icloud.com>
2025-11-21 15:01:37 +0400

    updated auth.py

--- auth.py
+++ auth.py
@@ -1,12 +1,6 @@
 import bcrypt
 import os
 
-#def hash_password(plain_text_password):
-    #password_bytes = plain_text_password.encode('utf-8')
- 	#salt = bcrypt.gensalt()
- 	#hashed_password = bcrypt.hashpw(password_bytes, salt)
-	#return hashed_password
-
 def hash_password(plain_text_password):
     password_bytes = plain_text_password.encode('utf-8')
     salt = bcrypt.gensalt()
