6862156f7e02ef28404f8be906c18c88ca8007de
Ashvinee <ashvineechatoo@icloud.com>
2025-11-21 20:07:19 +0400

    updated auth.py

--- auth.py
+++ auth.py
@@ -1,6 +1,8 @@
 import bcrypt
 import os
 
+USER_DATA_FILE = "users.txt"
+
 def hash_password(plain_text_password):
     password_bytes = plain_text_password.encode('utf-8')
     salt = bcrypt.gensalt()
@@ -26,3 +28,17 @@ print(f"\nVerification with correct password: {is_valid}")
 # Test verification with incorrect password
 is_invalid = verify_password("WrongPassword", hashed)
 print(f"Verification with incorrect password: {is_invalid}")
+
+def register_user(username, password):
+    if os.path.exists("users.txt"):
+        with open("users.txt", "r") as f:
+            for line in f:
+                stored_username, _ = line.strip().split(",", 1)
+                if stored_username == username:
+                    return False
+    hashed_password = hash_password(password)
+    with open("users.txt", "a") as f:
+        f.write(f"{username},{hashed_password}\n")
+    print(f"User '{username}' registered.")
+    return True
+
